dc1b9f3ea7446737804e4fb82f00e5aa663f7913
Brian <hoangdesu@gmail.com>
2024-02-18 12:47:10 +0700

    this is the beginning of something great

---
+++ LeetCode/1. Two Sum/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> seen = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int acc = target - nums[i];
+            if (seen.containsKey(acc)) {
+                return new int[]{i, seen.get(acc)};
+            } else {
+                seen.put(nums[i], i);
+            }
+        }
+        return new int[]{};
+    }
+}
