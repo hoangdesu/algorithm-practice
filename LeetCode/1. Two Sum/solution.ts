850152d43c06203ec9df42ee7cd3561f931aa424
hoangdesu <hoangdesu@gmail.com>
2024-08-12 01:56:19 +0700

    added typescript

---
+++ LeetCode/1. Two Sum/solution.ts
@@ -0,0 +1,11 @@
+function twoSum(nums: number[], target: number): number[] {
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) return [i, j];
+        }
+    }
+    return [] as number[];
+};
+
+// const test1 = [, 9]
+console.log(twoSum([2,7,11,15], 9));
