6773e89d4a2714e7ef15bbc6c10beb3eec594393
hoangdesu <hoangdesu@gmail.com>
2025-01-27 04:15:00 +0700

    20. valid parentheses java solution

---
+++ LeetCode/20. Valid Parentheses/Solution.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(isValid("([]) => {}"));
+        System.out.println(isValid("()[]{}"));
+        System.out.println(isValid("(]"));
+
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        HashMap<Character, Character> pairs = new HashMap<>();
+        pairs.put(')', '(');
+        pairs.put(']', '[');
+        pairs.put('}', '{');
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.add(c);
+                System.out.println("Current stack: " + stack);
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (stack.isEmpty())
+                    return false;
+                Character popped = stack.pop();
+                System.out.println("Popped: " + popped);
+                System.out.println("pair: " + pairs.get(c));
+                if (popped != pairs.get(c))
+                    return false;
+            }
+        }
+
+        System.out.println("Stack after: " + stack.toString());
+        if (!stack.isEmpty())
+            return false;
+
+        return true;
+
+    }
+}
